13541b0c249552ba4748b4d6214bed0c9ca702dd
Viacheslav Demchenko <demchenko_slava_@mail.ru>
2018-07-27 21:02:53 +0300

    Выполнил задание первого урока

---
+++ js/main.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+function myFirstApp(name,age) {
+
+	alert('Привет, меня зовут ' + name + ' и это моя первая программа!');
+
+	function showSkills() {
+		let skills = ['HTML5', 'CSS3', 'Photoshop', 'English'];
+		document.write('<b>' + 'Я владею:' + '</b>' + '<br>' + '<br>');
+
+		for (let i = 0; i < skills.length; i++) {
+			if(i == skills.length - 1) {
+				// В конце последнего элемента массива ставится точка
+      			document.write( (i + 1) + '. ' + skills[i] + '.' + '<br>');
+      		} else {
+      			/* В конце каждого элемента массива, кроме последнего 
+      			ставится точка с запятой */
+         		document.write( (i + 1) + '. ' + skills[i] + ';' + '<br>');
+      		}
+		}
+	}
+
+	showSkills();
+
+	function checkAge() {
+		if (age > 18) {
+			alert(name + ',' + ' Вы нам подходите.');
+		} else {
+			alert(name + ',' + ' Вы нам не подходите.');
+		}
+	}
+
+	checkAge()
+
+	function calcPow(num) {
+		result = num * num;
+	}
+
+	calcPow(5);
+	console.log(result);
+}
+
+myFirstApp('Вячеслав', 10);
+
+});
